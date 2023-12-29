53f9f8ef8743f77d4ad40956e60c1c7e25d54004
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-12-29 14:09:52 +0100

    Redirection order change

--- Syncfusion_HtmlToPdf/Program.cs
+++ Syncfusion_HtmlToPdf/Program.cs
@@ -2,10 +2,7 @@ using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -13,14 +10,13 @@ builder.Services.AddMediatR(typeof(Program));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
